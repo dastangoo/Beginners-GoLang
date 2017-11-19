a455818c7af9ab058545597256ccfb59949889cd
dastangoo <hamed.dastangoo@gmail.com>
2017-11-19 20:32:03 +0330

    String conversions

--- checkOS.go
+++ checkOS.go
@@ -2,10 +2,19 @@ package main
 
 import (
   "fmt"
+  "strings"
 )
 
 
 func main()  {
-    var ch int = 'a'
-    fmt.Println(unicode.IsLetter(ch))
+  // str := "Beginning of the string " + 
+  //     "continuation of the string"
+  // s := "hel" + "lo"
+  // s += " world"
+  // var str string = "This is just an example of a string"
+  // fmt.Printf("T/F? does the string \"%s\" has prefix %s?", str, "Th")
+  // fmt.Printf("%t\n", strings.hasPrefix(str, "Th"))
+  var str string = "Hi, I am Adam, Hi"
+  fmt.Printf("The position of \"marc\" is: ")
+  fmt.Printf("%d\n", strings.Index(str, "Marc"))
 }
