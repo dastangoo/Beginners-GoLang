6f02304b1ab1270cdd6e344a256adc7f1f1a724f
dastangoo <hamed.dastangoo@gmail.com>
2017-11-19 13:03:06 +0330

    Value  Reference Types

--- hello.go
+++ hello.go
@@ -1,45 +1,19 @@
 package main
 import (
   "fmt"
-  "os"
 )
 func main()  {
-  // var a, b *int
-  // var a int
-  // var b bool
-  // var str string
-
-  // var (
-  //   a int
-  //   b bool
-  //   str string
-  // )
+  // path := os.Getenv("PATH")
+  // fmt.Printf("PATH is: %s\n", path);  
+  // a := 50
+  // b := false
+  // a = 20
+  // var a string = "abc"
+  // var a b c = 2, 9 "abc"
+  a := 5
+  b := 10
+  a, b = b, a
   
-  // var a int = 15
-  // var i = 5
-  // var b bool = false
-  // var str string = "Go says hello to the world"
-  
-  // var a = 15
-  // var b = false
-  // var str = "Go says hello to the world"
-
-  // var (
-  //   a = 15
-  //   b = false
-  //   str = "Go says hello to the world"
-  //   numShips = 50
-  //   city string
-  // )
-  
-  // var (
-  //   HOME = os.Getenv("HOME")
-  //   USER = os.Getenv("USER")
-  //   GOROOT = os.Getenv("GOROOT")
-  // )
-  
-  // var goos string = os.Getenv("GOOS")
-  path := os.Getenv("PATH")
-  fmt.Printf("PATH is: %s\n", path);  
+  fmt.Println(a, b)
 }
 
