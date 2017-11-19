52684abe7b2307e04fce59e9ec0bea82e983b4c7
dastangoo <hamed.dastangoo@gmail.com>
2017-11-19 11:40:40 +0330

    Interaction with other languages

--- hello.go
+++ hello.go
@@ -1,7 +1,23 @@
-package main
+package print
 
-import "fmt"
+// #include <stdio.h>
+// #include <stdlib.h>
 
-func main() {
-	fmt.Printf("Hello World\n")
+import "C"
+import "unsafe"
+
+func Print(s string)  {
+  cs := C.CString(s)
+  defer C.free(unsafe.Pointer(cs))
+  C.fputs(cs, (*C.FILE(C.stdout)))
 }
+
+
+include $(GOROOT)/src/Make.inc
+
+TARG=rand
+CGOFILES =\
+     c1.go\
+     
+     
+include $(GOROOT)/src/Make.pkg
