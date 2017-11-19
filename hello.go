8833fa19caf32df38fa28eba52d2a8c6ed69b946
dastangoo <hamed.dastangoo@gmail.com>
2017-11-19 12:41:40 +0330

    Conversions and Constants

--- hello.go
+++ hello.go
@@ -1,22 +1,47 @@
 package main
-
-import (
-  "fmt"
-)
-
-const c = "C"
-var v int = 5
-type T struct{
-  
-}
-
-func init()  {
-  // Initialization of package
-}
-
+import "fmt"
 func main()  {
-  var a int 
-  Func1()
-  // ...
-  fmt.Println(a)
+  // const beef, two, c = "meat", 2, "veg"
+  // const Monday, Tuesday, Wednesday, Thursday, Friday, Saturday = 1, 2, 3, 4, 5, 6
+  // 
+  // 
+  // const (
+  //   Monday, Tuesday, Wednesday = 1, 2, 3
+  //   Thursday, Friday, Saturday = 4, 5, 6
+  // )
+  // 
+  // const (
+  //   unknown = 0
+  //   Female = 1
+  //   Male = 2
+  // )
+  // 
+  // const (
+  //   a = iota
+  //   b 
+  //   c
+  // )
+  // 
+  // const (
+  //   Sunday = iota
+  //   Monday
+  //   Tuesday
+  //   Wednesday
+  //   Thursday
+  //   Friday
+  //   Saturday
+  // )
+
+  type Color int 
+  const (
+    RED Color = iota // 0
+    ORANGE // 1
+    YELLOW // 2
+    GREEN // 3
+    BLUE // 4
+    INDIGO // 5
+    VIOLET // 6
+  )
+  fmt.Println(YELLOW);  
 }
+
