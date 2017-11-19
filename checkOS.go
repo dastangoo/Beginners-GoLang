91ad8fdd11b3651e7f9380bf47049e30f7ab22e9
dastangoo <hamed.dastangoo@gmail.com>
2017-11-19 19:15:46 +0330

    Numerical Types 3

--- checkOS.go
+++ checkOS.go
@@ -2,29 +2,28 @@ package main
 
 import (
   "fmt"
-  "math"
 )
 
 
-func Uint8FromInt(n int) (uint8, error) {
-  if 0 <= n && n <= math.MaxUint8 { // conversion is safe
-    return uint8(n), nil
-  }
-  return 0, fmt.Errorf("%d is out of the uint8 range", n)
-}
-func IntFromFloat64(x float64) int {
-  if math.MinInt32 <= x && x <= math.MaxInt32 { // x lies in the integer range
-    whole, fraction := math.Modf(x)
-    if fraction >= 0.5 {
-      whole++
-    }
-    return int(whole)
-  }
-  panic(fmt.Sprintf("%g is out of int32 range", x))
-}
 func main()  {
-    // fmt.Println(Uint8FromInt(100000000000))
-    // fmt.Println(IntFromFloat64(65.9))
-    var c1 complex64 = 5 + 10i
-    fmt.Printf("The value is: %v", c1)
+  type ByteSize float64
+  const (
+    _ = iota // ignore the first value by assigning to blank identifier
+    KB ByteSize = 1 << (10 * iota)
+    MB
+    GB
+    TB
+    PB
+    EB
+    ZB
+    YB
+  )
+  type BitFlag int
+  const (
+    Active BitFlag = 1 << iota // 1 << 0 == 1
+    Send                       // 1 << 1 == 2
+    Receive                    // 1 << 2 == 4
+  )
+  flag := Active | Send
+  fmt.Println(flag)
 }
