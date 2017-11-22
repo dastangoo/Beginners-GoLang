05848d9a801403574730f3d354a4822cb32cd25b
dastangoo <hamed.dastangoo@gmail.com>
2017-11-22 22:24:22 +0330

    If..else statement 3

--- example-01.go
+++ example-01.go
@@ -3,12 +3,18 @@ import "fmt"
 
 func main() {
   var first int = 10
-  // var cond int 
+  var cond int 
   if first <= 0 {
     fmt.Println("first is less than or equal to 0\n")
   } else if first > 0 && first <5 {
     fmt.Println("first is between 0 and 5\n")
   } else {
     fmt.Println("first is greater or equal to 5\n")
+  }
+  
+  if cond = 5; cond > 10 {
+    fmt.Println("cond is is greater than 10\n")
+  } else {
+    fmt.Println("cond is not greater than 10\n")
   }
 }
