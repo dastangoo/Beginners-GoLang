252eadfda2610799257fb718f9e74d5828389320
dastangoo <hamed.dastangoo@gmail.com>
2017-11-22 19:06:37 +0330

    String conversions 2

--- checkOS.go
+++ checkOS.go
@@ -2,19 +2,32 @@ package main
 
 import (
   "fmt"
-  "strings"
+  "strings" 
 )
 
 
-func main()  {
-  // str := "Beginning of the string " + 
-  //     "continuation of the string"
-  // s := "hel" + "lo"
-  // s += " world"
-  // var str string = "This is just an example of a string"
-  // fmt.Printf("T/F? does the string \"%s\" has prefix %s?", str, "Th")
-  // fmt.Printf("%t\n", strings.hasPrefix(str, "Th"))
-  var str string = "Hi, I am Adam, Hi"
-  fmt.Printf("The position of \"marc\" is: ")
-  fmt.Printf("%d\n", strings.Index(str, "Marc"))
+func main() {
+    // var str string = "hi, I am Adam, Hi"
+    // fmt.Printf("The position of \"marc\" is: ")
+    // fmt.Printf("%d\n", strings.Index(str, "Marc"))
+    // fmt.Printf("The position of the first instance of \"hi\" is: ")
+    // fmt.Printf("%d\n", strings.Index(str, "Hi"))
+    // fmt.Printf("The position of the last instance of \"hi\" is: ")
+    // fmt.Printf("%d\n", strings.LastIndex(str, "Hi"))
+    // fmt.Printf("The position of \"Burger\" is: ")
+    // fmt.Printf("%d\n", strings.Index(str, "Burger"))    
+    
+    
+    // strings.Replace(str, old, new, n)
+    strings.Replace("Hi, I am Adam, hi", "Hi", "Bye", 2)
+    
+    var str string = "hello, how is it going, Adam?"
+    var manyG = "ggggggg"
+    
+    fmt.Printf("Number of H's in %s: ", str)
+    fmt.Printf("%d\n", strings.Count(str, "H"))
+    
+    fmt.Printf("Number of double g's in %s is: ", manyG)
+    fmt.Printf("%d\n", strings.Count(manyG, "gg"))
+    
 }
